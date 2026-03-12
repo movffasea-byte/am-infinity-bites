c6821d61a987194ef10e4c707bd9cb16632e1c4c
Abraham Gaba <movffasea@gmail.com>
2026-03-12 13:51:13 +0100

    use product data to read products

---
+++ data/products.js
@@ -0,0 +1,36 @@
+const products = [
+    {
+      name: "Classic Fruit Parfait",
+      price: 2500,
+      img: "images/unnamed111.jpg"
+    },
+    {
+      name: "Healthy Fruit Blend",
+      price: 8000,
+      img: "images/ab15.jpg"
+    },
+    {
+      name: "Yogurt drowling bites",
+      price: 4000,
+      img: "images/ab10.jpg"
+    },
+    {
+      name: "fresh Salad Mix",
+      price: 20000,
+      img: "images/ab8.jpg"
+    },
+     {
+      name: "Bossy Special",
+      price: 6000,
+      img: "images/ab2.jpg"
+     },
+     {name : "Tropical Fruit Medley",
+      price: 5000,
+      img: "images/ab2.jpg"
+    },
+    {name : "Berry Blast Bowl",
+      price: 4500,
+      img: "images/ab2.jpg"
+
+    },
+    ];
--- script/a.mstarts.js
+++ script/a.mstarts.js
@@ -1,31 +1,4 @@
 
-const products = [
-    {
-      name: "Classic Fruit Parfait",
-      price: 2500,
-      img: "images/unnamed111.jpg"
-    },
-    {
-      name: "Healthy Fruit Blend",
-      price: 8000,
-      img: "images/ab15.jpg"
-    },
-    {
-      name: "Yogurt drowling bites",
-      price: 4000,
-      img: "images/ab10.jpg"
-    },
-    {
-      name: "fresh Salad Mix",
-      price: 20000,
-      img: "images/ab8.jpg"
-    },
-     {
-      name: "Bossy Special",
-      price: 6000,
-      img: "images/ab2.jpg"
-     }
-  ];
 
 
   
