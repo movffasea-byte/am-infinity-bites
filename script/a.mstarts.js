c564615691418ffd36005dd40edb735f48fb4be6
Abraham Gaba <movffasea@gmail.com>
2026-03-13 12:41:14 +0100

    make cart quantity interactive

--- script/a.mstarts.js
+++ script/a.mstarts.js
@@ -46,7 +46,14 @@
        });
       }
 
-       console.log(cart);
+      let cartQuantity = 0;
+
+      cart.forEach((item) => {
+      cartQuantity += item.quantity;
+      });
+
+      document.querySelector(`.js-cart-quantity`)
+      .innerHTML = cartQuantity;
        });
     });
 
